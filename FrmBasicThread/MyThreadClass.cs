86b60b249d16bca9782edb321bfb1817fd3118a2
19-anel <lyraresflovilla@gmail.ccom>
2025-09-24 19:25:31 +0800

    Add project files.

---
+++ FrmBasicThread/MyThreadClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace FrmBasicThread
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int loopCount = 1; loopCount <= 10; loopCount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
+
+               Thread.Sleep(1500);
+            }
+        }
+    }
+}
